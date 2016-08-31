d66289d585cc521a5a310fc8fe0d6c8eb022e351
rafariancho <rafariancho@gmail.com>
2016-08-31 23:46:52 +0100

    Validation
    Added more validation code, fixed the unobstrusive validation with
    bootstrap.

--- Butler/src/Butler/ViewModels/DishViewModel.cs
+++ Butler/src/Butler/ViewModels/DishViewModel.cs
@@ -13,11 +13,13 @@ namespace Butler.ViewModels
         public List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> DishTypes { set; get; }  
         public List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> ConsistencyTypes { set; get; }  
         [Required]
-        [MaxLength(200,ErrorMessage = "Name is too long")]
-        [MinLength(10,ErrorMessage = "Name is too short")]
-        public String Name { get; set; } 
-        public int Tuppers { get; set; }  
-        public int Type { get; set; }    
+        [StringLength(100, MinimumLength = 20,ErrorMessage = "Name must be 20 to 100 characters long")]
+        public String Name { get; set; }
+        [Required]
+        public int Tuppers { get; set; }
+        [Required]
+        public int Type { get; set; }
+        [Required]
         public int Consistency { get; set; } 
         public IFormFile Image { get; set; }
         public string ImageSrc { get; set; }
