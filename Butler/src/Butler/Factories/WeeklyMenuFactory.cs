83f5c2c63cdd3e18068a5aca6fdb3a4d82f3535a
rafariancho <rafariancho@gmail.com>
2016-09-04 23:09:39 +0100

    Switched the dependecy injection to use the actual weekly menu factory
    Added more data to the layout and fixed the dinners duplication.

--- Butler/src/Butler/Factories/WeeklyMenuFactory.cs
+++ Butler/src/Butler/Factories/WeeklyMenuFactory.cs
@@ -23,15 +23,13 @@ namespace Butler.Factories
             List<DailyMenu> weeklyMenu = new List<DailyMenu>();
             List<Dish> lunches4Tuppers = dishes.Where(x => x.Type == Models.Type.Lunch && x.Tuppers > 2).OrderBy(r => Guid.NewGuid()).Take(2).ToList();
             var lunches = dishes.Where(x=> x.Type == Models.Type.Lunch && (!lunches4Tuppers.Select(y => y.Id).Contains(x.Id))).OrderBy(r => Guid.NewGuid()).Take(7);
-            var dinners = dishes.Where(x=> x.Type == Models.Type.Dinner).OrderBy(r => Guid.NewGuid()).Take(7);
+            var dinners = dishes.Where(x=> x.Type == Models.Type.Dinner).OrderBy(r => Guid.NewGuid()).Take(7).ToList();
 
             //Duplicated lunches in alternate days
             for (int i = 0; i < 2; i++)
             {
-                menus[i] = new Menu() { Lunch = lunches4Tuppers[i], Dinner = dinners.FirstOrDefault() };
-                dinners = dinners.Skip(1);
-                menus[i +2] = new Menu() { Lunch = lunches4Tuppers[i], Dinner = dinners.FirstOrDefault() };
-                dinners = dinners.Skip(1);
+                menus[i] = new Menu() { Lunch = lunches4Tuppers[i], Dinner = dinners[i] };
+                menus[i +2] = new Menu() { Lunch = lunches4Tuppers[i], Dinner = dinners[i+2] };
             }
             
             //Rest of menus
@@ -39,13 +37,12 @@ namespace Butler.Factories
             {
                 var lunchQuery =  lunches.SkipWhile(x => x.Id == menus[j-1].Lunch.Id);
                 
-                menus[j] = new Menu() { Lunch = lunchQuery.FirstOrDefault(), Dinner = dinners.FirstOrDefault() };
-                dinners = dinners.Skip(1);
+                menus[j] = new Menu() { Lunch = lunchQuery.FirstOrDefault(), Dinner = dinners[j] };
             }
             
             for (int k = 0; k < 7; k++)
             {
-                DayOfWeek day = ((DayOfWeek)k + 1);
+                DayOfWeek day = k == 6 ? DayOfWeek.Sunday: ((DayOfWeek)k + 1);
                 weeklyMenu.Add(new DailyMenu() { Day = day.ToString(), Menu = menus[k]});
                 Debug.WriteLine("Debug.WriteLine" + menus[k].Lunch.Id.ToString());
             }
