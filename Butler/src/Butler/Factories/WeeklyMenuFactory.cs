478c209e0b275136cd549bbf9833e77b6c45591e
rafariancho <rafariancho@gmail.com>
2016-09-04 14:22:49 +0100

    Tests and development of the weely menu factory
    TDDing the way in...

--- Butler/src/Butler/Factories/WeeklyMenuFactory.cs
+++ Butler/src/Butler/Factories/WeeklyMenuFactory.cs
@@ -9,9 +9,46 @@ namespace Butler.Factories
 {
     public class WeeklyMenuFactory : IWeeklyMenuFactory
     {
+        private IRepository _repository;
+
+        public WeeklyMenuFactory(IRepository repository)
+        {
+            _repository = repository;
+        }
         public List<DailyMenu> GetWeeklyMenu()
         {
-            var weeklyMenu = new List<DailyMenu>();
+            List<Dish> dishes = _repository.GetDishes().ToList();
+            List<Menu> menus = new List<Menu>();
+            List<DailyMenu> weeklyMenu = new List<DailyMenu>();
+
+            var lunches = dishes.Where(x=> x.Type == Models.Type.Lunch).OrderBy(r => Guid.NewGuid()).Take(7);
+            var dinners = dishes.Where(x=> x.Type == Models.Type.Dinner).OrderBy(r => Guid.NewGuid()).Take(7);
+
+            for (int i = 0; i < 7; i++)
+            {
+                //If i > 0 check if menu is repeating 
+                var lunchQuery =  i>0 ? lunches.SkipWhile(x => x.Id == menus[i-1].Lunch.Id): lunches;
+
+                ////Check that the lunch is not repeting more than twice
+                //while (menus.Where(x => x.Lunch.Id == lunchQuery.FirstOrDefault().Id).Count() > 2) {
+                //    lunchQuery = lunchQuery.SkipWhile(x => x.Id == lunchQuery.FirstOrDefault().Id);
+                //}
+
+                var lunch =lunchQuery.FirstOrDefault();
+                var dinner = dinners.Skip(i).FirstOrDefault();
+
+                Menu menu = new Menu() { Lunch = lunch, Dinner = dinner };
+                menus.Add(menu);
+                DayOfWeek day = ((DayOfWeek)i + 1);
+                weeklyMenu.Add(new DailyMenu() { Day = day.ToString(), Menu = menu });
+            }
+            //weeklyMenu.Add(new DailyMenu() { Day = "Monday", Menu = menu });
+            //weeklyMenu.Add(new DailyMenu() { Day = "Tuesday", Menu = menu });
+            //weeklyMenu.Add(new DailyMenu() { Day = "Wednesday", Menu = menu });
+            //weeklyMenu.Add(new DailyMenu() { Day = "Thursday", Menu = menu });
+            //weeklyMenu.Add(new DailyMenu() { Day = "Friday", Menu = menu });
+            //weeklyMenu.Add(new DailyMenu() { Day = "Saturday", Menu = menu });
+            //weeklyMenu.Add(new DailyMenu() { Day = "Sunday", Menu = menu });
             return weeklyMenu;
         }
     }
---
+++ Butler/test/ButlerTests/Fakes/FakeRepository.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using Butler.Interfaces;
+using Butler.Models;
+
+namespace ButlerTests
+{
+    internal class FakeRepository : IRepository
+    {
+        private List<Dish> _list;
+
+        public void AddDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddIngredient(Ingredient ingredient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDish(int dishId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteIngredient(int ingredientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void InMemoryDishes(List<Dish> list)
+        {
+            _list = list;
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dish GetDish(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Dish> GetDishes()
+        {
+            return _list;
+        }
+
+        public IEnumerable<Dish> GetDishes(int start, int pagesize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Dish> GetDishes(string search, int start, int pagesize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDishesCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateIngredient(Ingredient ingredient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
